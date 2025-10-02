4c881c1b726c7eb86938b9e419b71be3136cd28d
Sarah_R <sranganadane@eugeniaschool.com>
2025-10-02 13:12:58 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+ #**MODULE 1**
+
+# TODO: Create an integer variable named 'age' with your age
+# TODO: Create a float variable named 'height' with your height in meters
+# TODO: Create a string variable named 'name' with your name
+# TODO: Create a boolean variable named 'is_student' and set it to True
+
+# Print all variables and their types
+# Example: print(f"Age: {age}, Type: {type(age)}")
+
+#question 1
+
+# age = 10
+# height = 1.04
+# name = 'sarah'
+# print(f"Age : {age}, type = {type(age)} ")
+# print(f"height : {height}, type = {type(height)} ")
+# print(f"name : {name}, type = {type(name)} ")
+# is_student = True
+# print(f"is_student : {is_student}, type = {type(is_student)} ")
+
+#question 2
+
+# TODO: Create two variables 'a' and 'b' with numeric values
+# TODO: Perform addition, subtraction, multiplication, and division of a and b
+# Print the results of each operation
+# TODO 2: Calculate the remainder of a divided by b using the modulo operator
+# TODO 3: Calculate a to the power of b using the exponentiation operator
+
+# a = 9
+# b = 3
+# print(int(a/b))
+# print(f"sous question 2) {a % b}")
+# print(f"sous question 3) {a**b}")
+
+# TODO: Concatenate the two names to create a 'full_name' variable
+# first_name = 'SARAH'
+# full_name = 'ranganadane'
+# print(first_name+full_name)
+# print(f" taille de first_name {len(first_name)}")
+# print(full_name.upper())
+
+# # TODO: Create a string with multiple words and split it into a list
+# phrase = "l'exercice du jour"
+# print(phrase.split())
+
+
+# TODO: Convert a string containing a number to an integer
+nb = '2'
+#print(int(nb))
+
+# TODO: Create two boolean variables
+# TODO: Perform AND, OR, and NOT operations on these variables
+# Print the results
+
+bool1 = True
+bool2 = False
+
+and_result = bool1 and bool2
+or_result = bool1 or bool2
+not_bool1 = not bool1
+not_bool2 = not bool2
+
+# print("AND:", and_result)
+# print("OR:", or_result)
+# print("NOT bool1:", not_bool1)
+# print("NOT bool2:", not_bool2)
+
+# TODO: Create two numeric variables and use comparison operators
+# (>, <, >=, <=, ==, !=) to compare them
+# Print the results of these comparisons
+
+
+# **MODULE 2**
+# TODO: Create a list of your favorite fruits
+# TODO: Add a new fruit to the end of the list
+# fruit = ['pomme', 'banane', 'poire']
+# fruit.append("cerise")
+# print(fruit)
+
+# # TODO: Remove the second fruit from the list
+# del fruit[2]
+# print(fruit)
+
+# # TODO: Sort the list alphabetically
+# fruit.sort()
+# print(fruit)
+
+# # TODO: Create a new list with the firt two fruits
+# top_fruit = fruit[ : 2 ]
+# print(top_fruit)
+
+
+#              EXERCICE 2 TUPLE
+# TODO: Create a tuple with your favorite colors
+color = ("bleu", "rouge", "rose", "beige")
+
+# TODO: Try to modify one of the colors (this should raise an error)
+#tuple # liste donc non modifiable
+# TODO: Count how many times a specific color appears in the tuple
+
+# TODO: Find the index of a specific color
+
+# TODO: Create a new tuple by concatenating two existing tuples
